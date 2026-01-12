88f1acfd6b428cf4f8040a1dbf75e7b7e871c6f2
Rup Kumar Biswas <rb636739@gmail.com>
2026-01-12 18:35:49 +0530

    Add makeSettle function for group settlements
    Added makeSettle function to handle group settlements.

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,11 @@
 import axios from 'axios'
-
+import configData from '../config.json'
 
 //const API = axios.create({ baseURL: 'http://localhost:3001'})
-const API = axios.create({ baseURL: ''})
+const API = axios.create({ 
+  baseURL: configData.API_BASE_URL,
+  withCredentials: true
+})
 
 const profile = JSON.parse(localStorage.getItem('profile'))
 
